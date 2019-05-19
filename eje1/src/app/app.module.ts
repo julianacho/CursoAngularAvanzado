d39cf71dadca5a4583b20fbf3783bc766832381f
fingergroup <fingergroupcol@gmail.com>
2019-05-19 08:20:58 -0500

    Finalizacion Seccion 3

---
+++ eje1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
